8c222d25b2cb7fcf2a4142257daae6eafa8e246c
Kevin Villalobos <kevinv15v@gmail.com>
2020-03-04 21:59:29 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,19 +1,19 @@
 CREATE TABLE projects (
   project_id INT PRIMARY KEY,
-  title text(20),
-  category VARCHAR(20),
-  funding_goal VARCHAR(20),
+  title text,
+  category text,
+  funding_goal text,
   start_date DATE,
   end_date DATE);
   
 CREATE TABLE users (
 user_id INT PRIMARY KEY,
-name VARCHAR(20),
-age VARCHAR(20));
+name text,
+age text));
 
 CREATE TABLE pledges (
 pledges_id INT PRIMARY KEY,
-amount VARCHAR(20),
+amount text,
 foreign KEY(user_id) references users(user_id),
 foreign KEY(project_id) references projects(project_id));
 
